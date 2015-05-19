31f215207dd3d6b749c6d28816bb01afb610d75d
Chris920 <cdtded1212@gmail.com>
2015-05-19 16:04:13 -0400

    touched up a few things

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,7 +1,36 @@
-	
-var ready = function() {
-	
+$(document).on('ready page:load', function() {
 
+	var calendar = $('.calendar'),
+	userId = calendar && calendar.data('user-id');
+
+	 function generateCalendar(){ 
+		calendar.fullCalendar({
+		header: {
+			left: 'title',
+			center: 'prev next',
+			right: 'today month,agendaWeek,agendaDay' 
+		},
+		
+		defaultView: "agendaWeek",
+
+		lang: currentLangCode,
+		weekNumbers: true,
+		displayEventEnd: true,
+		eventLimit: true,
+		eventOverlap: false,
+		overlap: false,
+		eventSources: [{
+			url: '/users/' + userId + '/availabilities.json',
+			// rendering: 'background',
+			rendering: 'background', 
+			color: '#6AA4C1',
+		}],
+	});
+};
+
+generateCalendar();
+
+	var currentLangCode = 'en';
 	// build the language selector's options
 	$.each($.fullCalendar.langs, function(langCode) {
 		$('#lang-selector').append(
@@ -12,15 +41,14 @@ var ready = function() {
 		);
 	});
 
+
 	// rerender the calendar when the selected option changes
 	$('#lang-selector').on('change', function() {
 		if (this.value) {
 			currentLangCode = this.value;
 			$('.calendar').fullCalendar('destroy');
-			renderCalendar();
+			generateCalendar();
 		}
-	});
-}
+	});	
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+});
