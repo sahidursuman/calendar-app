9776ffb22cf99f6eca6be1aba76ed0705e680705
zhangmyihua <michaelzhang2@msn.com>
2015-06-15 17:00:13 -0400

    added default empty [] to model

--- db/migrate/20150615202605_create_repeating_availabilities.rb
+++ db/migrate/20150615202605_create_repeating_availabilities.rb
@@ -3,7 +3,7 @@ class CreateRepeatingAvailabilities < ActiveRecord::Migration
     create_table :repeating_availabilities do |t|
       t.integer :start_time
       t.integer :end_time
-      t.array :days_of_the_week
+      t.array :days_of_the_week, default: []
       t.integer :instructor_id
 
       t.timestamps null: false
