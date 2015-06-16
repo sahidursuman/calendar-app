335ee83e0dc1e20ff25affdfb39a9fe75bf1a101
zhangmyihua <michaelzhang2@msn.com>
2015-06-16 16:25:42 -0400

    added a method to convert days of the week to integers before it saves

--- app/models/repeating.rb
+++ app/models/repeating.rb
@@ -3,4 +3,11 @@ class Repeating < ActiveRecord::Base
   has_many :bookings
   serialize :days_of_the_week
 
+  before_create :arrayify
+
+private 
+  def arrayify
+    self.days_of_the_week.map! {|value| value.to_i}
+  end
+
 end
