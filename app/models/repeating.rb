4b7d4888dfc7801ae95676225c4926cd1e8994f4
zhangmyihua <michaelzhang2@msn.com>
2015-06-17 14:30:39 -0400

    created two arrays that hold dates for the start and end times.

--- app/models/repeating.rb
+++ app/models/repeating.rb
@@ -3,10 +3,14 @@ class Repeating < ActiveRecord::Base
   has_many :bookings
   serialize :days_of_the_week
 
+
+
   before_create :arrayify
 
 def day_checker
   dow = []
+  start_dates = []
+  end_dates = []
   if self.days_of_the_week.present?
     self.days_of_the_week.each do |day|
       if day == 0
@@ -16,11 +20,16 @@ def day_checker
       end
     end
   end
-  return dow 
+  dow.each do |x|
+    start_dates << self.start_time.change(day: x)
+    end_dates << self.end_time.change(day: x)
+  end
 end
 
 
 private 
+
+# This method must exist for the calendar to read for days of the week(dow)
   def arrayify
     if self.days_of_the_week.present? 
       self.days_of_the_week.map! {|value| value.to_i}
