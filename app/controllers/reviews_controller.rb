5c048f45feed2c4f7d2128b43f1b2b1399b6b322
zhangmyihua <michaelzhang2@msn.com>
2015-06-23 04:43:24 +0800

    filled in controller with action details

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,48 @@
 class ReviewsController < ApplicationController
+  before_filter :load_instructor
+  before_filter :ensure_logged_in, only: [:create, :destroy]
 
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @review = @instructor.reviews.build(review_params)
+    @review.user = current_user
+    load_instructor
+    if @review.save
+      redirect_to product_path(@instructor), notice: 'Review created successfully'
+    else
+      render 'products/show'
+    end
+  end
+
+  def edit
+  @review = Review.find(params[:id])
+  load_instructor
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update_attributes(review_params)
+      redirect_to product_path(@instructor)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+ private
+  def review_params
+    params.require(:review).permit(:comment, :product_id)
+  end
+
+  def load_instructor
+    @instructor = instructor.find(params[:product_id])
+  end
 end
