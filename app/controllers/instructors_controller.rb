5ee96488525130d0d29711dfd0e8dd9cb1500da3
zhangmyihua <michaelzhang2@msn.com>
2015-06-23 05:01:08 +0800

    adding review display and form to instructor show

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -7,6 +7,7 @@ class InstructorsController < ApplicationController
   def show
     @instructor = Instructor.find(params[:id])
     @availabilities = @instructor.availabilities
+    @reviews = @instructor.reviews
     @first_name = @instructor.teacher.first_name.capitalize
   end
 
