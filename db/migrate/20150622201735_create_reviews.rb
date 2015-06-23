c997fe5fe6c2dd108d357b24fcf6060577d70a08
zhangmyihua <michaelzhang2@msn.com>
2015-06-24 02:47:55 +0800

    adding review seeds

--- db/migrate/20150622201735_create_reviews.rb
+++ db/migrate/20150622201735_create_reviews.rb
@@ -2,7 +2,6 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.string :student_id
-      t.string :integer
       t.integer :instructor_id
       t.text :comment
       t.integer :rating
